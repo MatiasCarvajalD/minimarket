1e65164c9b650022af6153ede3d49476b14cf329
MatiasCarvajalD <127509542+MatiasCarvajalD@users.noreply.github.com>
2024-06-26 10:14:04 -0400

    Update BebidasTableSeeder.php

--- database/seeders/BebidasTableSeeder.php
+++ database/seeders/BebidasTableSeeder.php
@@ -14,7 +14,7 @@ class BebidasTableSeeder extends Seeder
         
         Bebida::create(['nombre' => 'Coca-Cola', 'litros' => '500 ml', 'precio' => 900, 'imagen' => 'Coca-Cola (500 ml).jpg']);
         Bebida::create(['nombre' => 'Coca-Cola', 'litros' => '1 litro', 'precio' => 1200, 'imagen' => 'Coca-Cola (1 litro).jpg']);
-        Bebida::create(['nombre' => 'Coca-Cola', 'litros' => '1.5 litros', 'precio' => 1500, 'imagen' => 'Coca-Cola (1.5 litros).jpg']);
+        Bebida::create(['nombre' => 'Coca-Cola', 'litros' => '1.5 litros', 'precio' => 1500, 'imagen' => 'Coca-Cola (1.5 litros).png']);
         Bebida::create(['nombre' => 'Coca-Cola', 'litros' => '3 litros', 'precio' => 2500, 'imagen' => 'Coca-Cola (3 litros).jpg']);
         
         Bebida::create(['nombre' => 'Pepsi', 'litros' => '600 ml', 'precio' => 850, 'imagen' => 'Pepsi (600 ml).jpg']);
@@ -25,10 +25,9 @@ class BebidasTableSeeder extends Seeder
         Bebida::create(['nombre' => 'Fanta', 'litros' => '1.5 litros', 'precio' => 1500, 'imagen' => 'Fanta (1.5 litros).jpg']);
         Bebida::create(['nombre' => 'Fanta', 'litros' => '3 litros', 'precio' => 2500, 'imagen' => 'Fanta (3 litros).jpg']);
         
-        Bebida::create(['nombre' => 'Sprite', 'litros' => '500 ml', 'precio' => 900, 'imagen' => 'sprite.jpg']);
-        Bebida::create(['nombre' => 'Sprite', 'litros' => '1 litro', 'precio' => 1200, 'imagen' => 'sprite.jpg']);
-        Bebida::create(['nombre' => 'Sprite', 'litros' => '1.5 litros', 'precio' => 1500, 'imagen' => 'sprite.jpg']);
-        Bebida::create(['nombre' => 'Sprite', 'litros' => '3 litros', 'precio' => 2500, 'imagen' => 'sprite.jpg']);
+        Bebida::create(['nombre' => 'Sprite', 'litros' => '591 ml', 'precio' => 900, 'imagen' => 'Sprite (591 ml).jpg']);
+        Bebida::create(['nombre' => 'Sprite', 'litros' => '1.5 litros', 'precio' => 1500, 'imagen' => 'Sprite (1.5 litro).jpg']);
+        Bebida::create(['nombre' => 'Sprite', 'litros' => '3 litros', 'precio' => 2500, 'imagen' => 'Sprite (3 litros).jpg']);
         
         Bebida::create(['nombre' => 'Bebida energética Red Bull', 'litros' => '250 ml', 'precio' => 2000, 'imagen' => 'Bebida energética Red Bull (250 ml).jpg']);
         Bebida::create(['nombre' => 'Bebida energética Red Bull', 'litros' => '355 ml', 'precio' => 2800, 'imagen' => 'Bebida energética Red Bull (355 ml).jpg']);
@@ -39,12 +38,12 @@ class BebidasTableSeeder extends Seeder
         Bebida::create(['nombre' => 'Bebida energética Score Gorilla', 'litros' => '600 ml', 'precio' => 1800, 'imagen' => 'Bebida energética Score Gorilla (500 ml).jpg']);
         
         Bebida::create(['nombre' => 'Agua mineral Cachantún', 'litros' => '500 ml', 'precio' => 700, 'imagen' => 'Agua mineral Cachantún (500 ml).jpg']);
-        Bebida::create(['nombre' => 'Agua mineral Cachantún', 'litros' => '1 litro', 'precio' => 900, 'imagen' => 'Agua mineral Cachantún (1.5 litros).jpg']);
-        Bebida::create(['nombre' => 'Agua mineral Cachantún', 'litros' => '1.5 litros', 'precio' => 1000, 'imagen' => 'Agua mineral Cachantún (2 litros).jpg']);
+        Bebida::create(['nombre' => 'Agua mineral Cachantún', 'litros' => '1 litro', 'precio' => 900, 'imagen' => 'Agua mineral Cachantún (1 litros).jpg']);
+        Bebida::create(['nombre' => 'Agua mineral Cachantún', 'litros' => '1.5 litros', 'precio' => 1000, 'imagen' => 'Agua mineral Cachantún (1.5 litros).jpg']);
         Bebida::create(['nombre' => 'Agua mineral Cachantún', 'litros' => '2.5 litros', 'precio' => 1200, 'imagen' => 'Agua mineral Cachantún (2.5 litros).jpg']);
         
         Bebida::create(['nombre' => 'Agua mineral Vital', 'litros' => '600 ml', 'precio' => 650, 'imagen' => 'Agua mineral Vital (600 ml).jpg']);
-        Bebida::create(['nombre' => 'Agua mineral Vital', 'litros' => '990 ml', 'precio' => 850, 'imagen' => 'Agua mineral Vital (990 ml)l.jpg']);
+        Bebida::create(['nombre' => 'Agua mineral Vital', 'litros' => '990 ml', 'precio' => 850, 'imagen' => 'Agua mineral Vital (990 ml).jpg']);
         Bebida::create(['nombre' => 'Agua mineral Vital', 'litros' => '1.5 litros', 'precio' => 900, 'imagen' => 'Agua mineral Vital (1.5 litros).jpg']);
         
         Bebida::create(['nombre' => 'Jugos Watts', 'litros' => '200 ml', 'precio' => 500, 'imagen' => 'Jugos Watts (200 ml).jpg']);
@@ -52,7 +51,7 @@ class BebidasTableSeeder extends Seeder
         
         Bebida::create(['nombre' => 'Jugos Andina', 'litros' => '200 ml', 'precio' => 450, 'imagen' => 'Jugos Andina (200 ml).jpg']);
         Bebida::create(['nombre' => 'Jugos Andina', 'litros' => '600 ml', 'precio' => 850, 'imagen' => 'Jugos Andina (600 ml).jpg']);
-        Bebida::create(['nombre' => 'Jugos Andina', 'litros' => '1 litro', 'precio' => 1100, 'imagen' => 'Jugos Andina (1 litro).jpg']);
+        Bebida::create(['nombre' => 'Jugos Andina', 'litros' => '1.5 litro', 'precio' => 1500, 'imagen' => 'Jugos Andina (1.5 litro).jpg']);
         
         Bebida::create(['nombre' => 'Té helado Lipton', 'litros' => '400 ml', 'precio' => 900, 'imagen' => 'Té helado Lipton (400 ml).jpg']);
         Bebida::create(['nombre' => 'Té helado Lipton', 'litros' => '1.5 litros', 'precio' => 1500, 'imagen' => 'Té helado Lipton (1.5 litro).jpg']);
